e2bd9df5403d95772b78d565b234f9d07317fa73
michi <michi.blum@hotmail.com>
2022-03-24 23:06:39 +0100

    inject axios service and prepare endpoint for highlighting

--- web-api/src/app.controller.ts
+++ web-api/src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  /*
+  In here we can create new routes as needed. check the nest.js documentation for details.
+  */
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  defaultRouter(): string {
+    return this.appService.defaultRouter();
+  }
+
+  @Post('/highlight')
+  highlight(@Body() code: string): Promise<any> {
+    return this.appService.highlight(code);
   }
 }
--- web-api/src/app.service.ts
+++ web-api/src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private httpService: HttpService) {}
+  
+  defaultRouter(): string {
+    return 'Congrats, you reached the defaultRouter of our API! 🎉 Here will nothing happen, but at least you know that communication works :)';
+  }
+
+  async highlight(code): Promise<any> {
+    console.log(code)
+    return 'here we do syntax highlighting.';
   }
 }
