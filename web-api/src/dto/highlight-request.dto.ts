45fbe327d1bf8370ff3449949976d94f5f02f4b9
Sebastian Richner <sebastian@richner.ag>
2022-03-26 14:45:02 +0100

    Add validation for the request dto, add global validation pipe

--- web-api/src/dto/highlight-request.dto.ts
+++ web-api/src/dto/highlight-request.dto.ts
@@ -1,6 +1,19 @@
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
 import { SupportedLanguage } from '../constants/supported-languages.enum';
 
 export class HighlightRequestDto {
+
+  @IsNotEmpty()
+  @IsString()
   code: string;
+
+  @IsNotEmpty()
+  @IsEnum(SupportedLanguage, {
+    message: `language is not supported. Supported languages are ${
+      Object.keys(SupportedLanguage)
+        .map(key => SupportedLanguage[key])
+        .join(', ')
+    }`
+  })
   language: SupportedLanguage
 }
--- web-api/src/main.ts
+++ web-api/src/main.ts
@@ -1,9 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   const config: ConfigService = app.get(ConfigService);
 
   await app.listen(config.get('service.port'));
