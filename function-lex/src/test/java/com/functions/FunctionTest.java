17e29001fb5961fa3190d4092548133c35c10455
miczed <mighty.miczed@gmail.com>
2022-03-27 21:05:55 +0200

    Add unit tests for different language request

--- function-lex/src/test/java/com/functions/FunctionTest.java
+++ function-lex/src/test/java/com/functions/FunctionTest.java
@@ -5,7 +5,9 @@
  */
 package com.functions;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.azure.functions.*;
+import org.apache.commons.text.StringEscapeUtils;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
@@ -24,19 +26,94 @@ import static org.mockito.Mockito.*;
 public class FunctionTest {
 
     /**
-     * Unit test for Lex method.
+     * Unit test for Lex method with Java language.
      */
     @Test
     public void testLexJava() throws Exception {
+        HttpResponseMessage ret = sendLexRequest(
+            "java",
+            "public static void main(String[] args) {}"
+        );
+
+        // Verify
+        // TODO: currently we only verify that there are values in the body
+        //  and that they can be deserialized to an integer list, not that the values are correct
+
+        assertEquals(ret.getStatus(), HttpStatus.OK);
+        assertNotNull(ret.getBody());
+        assertNotEquals("", ret.getBody());
+
+        ObjectMapper mapper = new ObjectMapper();
+        List token_ids = mapper.readValue(ret.getBody().toString(), List.class);
+        assertNotEquals(0, token_ids.size());
+    }
+
+    /**
+     * Unit test for Lex method with Java language.
+     */
+    @Test
+    public void testLexKotlin() throws Exception {
+        HttpResponseMessage ret = sendLexRequest(
+            "kotlin",
+            "fun main() { println(\"Hello, World!\") }"
+        );
+
+        // Verify
+        // TODO: currently we only verify that there are values in the body
+        //  and that they can be deserialized to an integer list, not that the values are correct
+
+        assertEquals(ret.getStatus(), HttpStatus.OK);
+        assertNotNull(ret.getBody());
+        assertNotEquals("", ret.getBody());
+
+        ObjectMapper mapper = new ObjectMapper();
+        List token_ids = mapper.readValue(ret.getBody().toString(), List.class);
+        assertNotEquals(0, token_ids.size());
+    }
+
+    /**
+     * Unit test for Lex method with Java language.
+     */
+    @Test
+    public void testLexPython() throws Exception {
+        HttpResponseMessage ret = sendLexRequest(
+            "python",
+            "print(\"Hello World\")"
+        );
+
+        // Verify
+        // TODO: currently we only verify that there are values in the body
+        //  and that they can be deserialized to an integer list, not that the values are correct
+
+        assertEquals(ret.getStatus(), HttpStatus.OK);
+        assertNotNull(ret.getBody());
+        assertNotEquals("", ret.getBody());
+
+        ObjectMapper mapper = new ObjectMapper();
+        List token_ids = mapper.readValue(ret.getBody().toString(), List.class);
+        assertNotEquals(0, token_ids.size());
+    }
+
+
+    /**
+     * Sends a request to the lexing function
+     * @param language the language for the request
+     * @param code the source code
+     * @return the http response of the function
+     */
+    private HttpResponseMessage sendLexRequest(String language, String code) {
         // Setup
         @SuppressWarnings("unchecked")
         final HttpRequestMessage<Optional<String>> req = mock(HttpRequestMessage.class);
 
         final Map<String, String> queryParams = new HashMap<>();
-        queryParams.put("code", "public void testLexJava() throws Exception {}");
         doReturn(queryParams).when(req).getQueryParameters();
 
-        final Optional<String> queryBody = Optional.empty();
+        code = StringEscapeUtils.escapeJson(code);
+
+        String body = "{ \"lang_name\": \"" + language + "\", \"code\": \"" + code + "\" }";
+
+        final Optional<String> queryBody = Optional.of(body);
         doReturn(queryBody).when(req).getBody();
 
         doAnswer(new Answer<HttpResponseMessage.Builder>() {
@@ -53,13 +130,7 @@ public class FunctionTest {
         // Invoke
         final HttpResponseMessage ret = new Function().lex(req, context);
 
-        // Verify
-        // TODO: currently we only verify that there are values in the body, not that the values are correct
-
-        assertEquals(ret.getStatus(), HttpStatus.OK);
-        assertNotNull(ret.getBody());
-        assertNotEquals("", ret.getBody());
-
+        return ret;
     }
 
 }
