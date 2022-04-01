cf456d209a4c1cc50f090af413db00fdebb6ed34
Sebastian Richner <sebastian@richner.ag>
2022-04-01 22:08:14 +0200

    Add Spring Boot application for annotation service

---
+++ annotation-service/src/main/java/com/hack3rz/annotationservice/AnnotationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hack3rz.annotationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnotationServiceApplication.class, args);
+    }
+
+}
