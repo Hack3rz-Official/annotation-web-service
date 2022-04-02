d7e8d9d9146d67f0f69dd47b381d483ed5e3911c
Sebastian Richner <sebastian@richner.ag>
2022-04-02 10:47:41 +0200

    Add unit tests for incomplete code

--- annotation-service/src/test/java/com/hack3rz/annotationservice/service/AnnotationServiceTest.java
+++ annotation-service/src/test/java/com/hack3rz/annotationservice/service/AnnotationServiceTest.java
@@ -45,4 +45,31 @@ class AnnotationServiceTest {
 
         assertEquals(expectedLexingResult, annotationService.annotateCode(code, language));
     }
+
+    @Test
+    void whenIncompleteJavaCode_thenReturnLexedTokens() {
+        String code = "public static void mai}";
+        SupportedLanguage language = JAVA;
+        String expectedLexingResult = "{ startIndex=0, endIndex=5, tokenId=35 }, { startIndex=7, endIndex=12, tokenId=38 }, { startIndex=14, endIndex=17, tokenId=48 }, { startIndex=19, endIndex=21, tokenId=102 }, { startIndex=22, endIndex=22, tokenId=60 }";
+
+        assertEquals(expectedLexingResult, annotationService.annotateCode(code, language));
+    }
+
+    @Test
+    void whenIncompleteKotlinCode_thenReturnLexedTokens() {
+        String code = "fun main() {";
+        SupportedLanguage language = KOTLIN;
+        String expectedLexingResult = "{ startIndex=0, endIndex=2, tokenId=74 }, { startIndex=3, endIndex=3, tokenId=4 }, { startIndex=4, endIndex=7, tokenId=146 }, { startIndex=8, endIndex=8, tokenId=9 }, { startIndex=9, endIndex=9, tokenId=10 }, { startIndex=10, endIndex=10, tokenId=4 }, { startIndex=11, endIndex=11, tokenId=13 }";
+
+        assertEquals(expectedLexingResult, annotationService.annotateCode(code, language));
+    }
+
+    @Test
+    void whenIncompletePython3Code_thenReturnLexedTokens() {
+        String code = "print(";
+        SupportedLanguage language = PYTHON;
+        String expectedLexingResult = "{ startIndex=0, endIndex=4, tokenId=42 }, { startIndex=5, endIndex=5, tokenId=54 }";
+
+        assertEquals(expectedLexingResult, annotationService.annotateCode(code, language));
+    }
 }
