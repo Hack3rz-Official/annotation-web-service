50d13a2faac4e3886f44127134f71e7ba08fe655
miczed <mighty.miczed@gmail.com>
2022-04-05 18:09:36 +0200

    Connect annotation-service to mongodb to store annotations

--- annotation-service/src/main/java/com/hack3rz/annotationservice/service/AnnotationService.java
+++ annotation-service/src/main/java/com/hack3rz/annotationservice/service/AnnotationService.java
@@ -1,24 +1,23 @@
 package com.hack3rz.annotationservice.service;
 
-import static com.hack3rz.annotationservice.enumeration.SupportedLanguage.KOTLIN;
-import static com.hack3rz.annotationservice.enumeration.SupportedLanguage.PYTHON;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
+import com.hack3rz.annotationservice.enumeration.SupportedLanguage;
+import com.hack3rz.annotationservice.exception.ApiException;
+import lexer.HTok;
+import lexer.LTok;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
-import com.hack3rz.annotationservice.enumeration.SupportedLanguage;
-import com.hack3rz.annotationservice.exception.ApiException;
-
-import lexer.LTok;
 import resolver.JavaResolver;
 import resolver.KotlinResolver;
 import resolver.Python3Resolver;
 import resolver.Resolver;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static com.hack3rz.annotationservice.enumeration.SupportedLanguage.KOTLIN;
+import static com.hack3rz.annotationservice.enumeration.SupportedLanguage.PYTHON;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 /**
  * Service for providing the lexed tokens
@@ -27,7 +26,7 @@ import resolver.Resolver;
 public class AnnotationService {
     private static final Logger log = LoggerFactory.getLogger(AnnotationService.class);
 
-    public String annotateCode(String code, SupportedLanguage language) {
+    public LTok[] annotateCode(String code, SupportedLanguage language) {
         log.info("Received code to annotate...");
 
         Resolver resolver = getResolverByLanguage(language);
@@ -42,7 +41,50 @@ public class AnnotationService {
 
         log.info("LexingToken length = {}", lexingTokens.length);
 
-        return Arrays.stream(lexingTokens).map(this::mapLTok).collect(Collectors.joining(", "));
+        return lexingTokens;
+    }
+
+    public HTok[] highlightCode(String code, SupportedLanguage language) {
+        log.info("Received code to highlight...");
+
+        Resolver resolver = getResolverByLanguage(language);
+        log.info("Instantiated resolver of type {}", resolver.getClass());
+
+        HTok[] highlightTokens = resolver.highlight(code);
+
+        if (highlightTokens == null) {
+            log.error("Code could not be highlighted.");
+            throw new ApiException(BAD_REQUEST, "Code could not be highlighted.", "highlightTokens was null");
+        }
+
+        log.info("HighlightToken length = {}", highlightTokens.length);
+        
+        return highlightTokens;
+    }
+
+    public String debugHighlightCode(String code, SupportedLanguage language) {
+        log.info("Received code to highlight...");
+
+        Resolver resolver = getResolverByLanguage(language);
+        log.info("Instantiated resolver of type {}", resolver.getClass());
+
+        String htmlCode = resolver.debug(code);
+
+        if (htmlCode == null) {
+            log.error("Code could not be highlighted.");
+            throw new ApiException(BAD_REQUEST, "Code could not be highlighted.", "htmlCode was null");
+        }
+
+        return htmlCode;
+    }
+
+    public String serializeLexingTokens(LTok[] lToks) {
+        return Arrays.stream(lToks).map(this::mapLTok).collect(Collectors.joining(", "));
+    }
+
+    public String serializeHighlightTokens(HTok[] hToks) {
+        // TODO: check appropriate format for it
+        return Arrays.stream(hToks).map(this::mapLTok).collect(Collectors.joining(", "));
     }
 
     private Resolver getResolverByLanguage(SupportedLanguage language) {
--- annotation-service/src/test/java/com/hack3rz/annotationservice/service/AnnotationServiceTest.java
+++ annotation-service/src/test/java/com/hack3rz/annotationservice/service/AnnotationServiceTest.java
@@ -1,13 +1,11 @@
 package com.hack3rz.annotationservice.service;
 
-import static com.hack3rz.annotationservice.enumeration.SupportedLanguage.JAVA;
-import static com.hack3rz.annotationservice.enumeration.SupportedLanguage.KOTLIN;
-import static com.hack3rz.annotationservice.enumeration.SupportedLanguage.PYTHON;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static com.hack3rz.annotationservice.enumeration.SupportedLanguage.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AnnotationServiceTest {
 
@@ -23,7 +21,7 @@ class AnnotationServiceTest {
         String code = "public static void main(String[] args) {}";
         String expectedLexingResult = "{ startIndex=0, endIndex=5, tokenId=35 }, { startIndex=7, endIndex=12, tokenId=38 }, { startIndex=14, endIndex=17, tokenId=48 }, { startIndex=19, endIndex=22, tokenId=102 }, { startIndex=23, endIndex=23, tokenId=57 }, { startIndex=24, endIndex=29, tokenId=102 }, { startIndex=30, endIndex=30, tokenId=61 }, { startIndex=31, endIndex=31, tokenId=62 }, { startIndex=33, endIndex=36, tokenId=102 }, { startIndex=37, endIndex=37, tokenId=58 }, { startIndex=39, endIndex=39, tokenId=59 }, { startIndex=40, endIndex=40, tokenId=60 }";
 
-        assertEquals(expectedLexingResult, annotationService.annotateCode(code, JAVA));
+        assertEquals(expectedLexingResult, annotationService.serializeLexingTokens(annotationService.annotateCode(code, JAVA)));
     }
 
     @Test
@@ -31,7 +29,7 @@ class AnnotationServiceTest {
         String code = "fun main() { println(\"Hello, World!\") }";
         String expectedLexingResult = "{ startIndex=0, endIndex=2, tokenId=74 }, { startIndex=3, endIndex=3, tokenId=4 }, { startIndex=4, endIndex=7, tokenId=146 }, { startIndex=8, endIndex=8, tokenId=9 }, { startIndex=9, endIndex=9, tokenId=10 }, { startIndex=10, endIndex=10, tokenId=4 }, { startIndex=11, endIndex=11, tokenId=13 }, { startIndex=12, endIndex=12, tokenId=4 }, { startIndex=13, endIndex=19, tokenId=146 }, { startIndex=20, endIndex=20, tokenId=9 }, { startIndex=21, endIndex=21, tokenId=149 }, { startIndex=22, endIndex=34, tokenId=160 }, { startIndex=35, endIndex=35, tokenId=158 }, { startIndex=36, endIndex=36, tokenId=10 }, { startIndex=37, endIndex=37, tokenId=4 }, { startIndex=38, endIndex=38, tokenId=14 }";
 
-        assertEquals(expectedLexingResult, annotationService.annotateCode(code, KOTLIN));
+        assertEquals(expectedLexingResult, annotationService.serializeLexingTokens(annotationService.annotateCode(code, KOTLIN)));
     }
 
     @Test
@@ -39,7 +37,7 @@ class AnnotationServiceTest {
         String code = "print(\"Hello World\")";
         String expectedLexingResult = "{ startIndex=0, endIndex=4, tokenId=42 }, { startIndex=5, endIndex=5, tokenId=54 }, { startIndex=6, endIndex=18, tokenId=3 }, { startIndex=19, endIndex=19, tokenId=55 }";
 
-        assertEquals(expectedLexingResult, annotationService.annotateCode(code, PYTHON));
+        assertEquals(expectedLexingResult, annotationService.serializeLexingTokens(annotationService.annotateCode(code, PYTHON)));
     }
 
     @Test
@@ -47,7 +45,7 @@ class AnnotationServiceTest {
         String code = "public static void mai}";
         String expectedLexingResult = "{ startIndex=0, endIndex=5, tokenId=35 }, { startIndex=7, endIndex=12, tokenId=38 }, { startIndex=14, endIndex=17, tokenId=48 }, { startIndex=19, endIndex=21, tokenId=102 }, { startIndex=22, endIndex=22, tokenId=60 }";
 
-        assertEquals(expectedLexingResult, annotationService.annotateCode(code, JAVA));
+        assertEquals(expectedLexingResult, annotationService.serializeLexingTokens(annotationService.annotateCode(code, JAVA)));
     }
 
     @Test
@@ -55,7 +53,7 @@ class AnnotationServiceTest {
         String code = "fun main() {";
         String expectedLexingResult = "{ startIndex=0, endIndex=2, tokenId=74 }, { startIndex=3, endIndex=3, tokenId=4 }, { startIndex=4, endIndex=7, tokenId=146 }, { startIndex=8, endIndex=8, tokenId=9 }, { startIndex=9, endIndex=9, tokenId=10 }, { startIndex=10, endIndex=10, tokenId=4 }, { startIndex=11, endIndex=11, tokenId=13 }";
 
-        assertEquals(expectedLexingResult, annotationService.annotateCode(code, KOTLIN));
+        assertEquals(expectedLexingResult, annotationService.serializeLexingTokens(annotationService.annotateCode(code, KOTLIN)));
     }
 
     @Test
@@ -63,13 +61,13 @@ class AnnotationServiceTest {
         String code = "print(";
         String expectedLexingResult = "{ startIndex=0, endIndex=4, tokenId=42 }, { startIndex=5, endIndex=5, tokenId=54 }";
 
-        assertEquals(expectedLexingResult, annotationService.annotateCode(code, PYTHON));
+        assertEquals(expectedLexingResult, annotationService.serializeLexingTokens(annotationService.annotateCode(code, PYTHON)));
     }
 
     @Test
     void whenJavaCodeCanNotBeLexed_thenFailGracefully() {
         String code = "\"{ emptyResult }";
 
-        assertTrue(annotationService.annotateCode(code, JAVA).isEmpty());
+        assertTrue(annotationService.serializeLexingTokens(annotationService.annotateCode(code, JAVA)).isEmpty());
     }
 }
