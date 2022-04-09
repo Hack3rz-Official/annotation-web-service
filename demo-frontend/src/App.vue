54f14b14a9daf759a06fbd1de42c7ce198589f38
michi <michi.blum@hotmail.com>
2022-04-09 11:41:41 +0200

    init vue+tailwind demo frontend

---
+++ demo-frontend/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <header class="container mx-auto flex items-center justify-between py-5 px-3 bg-slate-300">
+    <div>
+      { Annotation Web Service }
+    </div>
+    <div class="">
+      by Hack3rZ
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+
+</style>
---
+++ demo-frontend/src/views/HomeView.vue
@@ -0,0 +1,44 @@
+<script setup>
+</script>
+
+<template>
+  <main class="container mx-auto px-3 gap-y-3">
+    <!-- <h1 class="text-2xl py-4">Syntax Highlighting</h1> -->
+    <p class="py-4">
+      Highlight your Java, Python and Kotlin code with our Annotation Web
+      Service. Highlight your Java, Python and Kotlin code with our Annotation
+      Web Service. Highlight your Java, Python and Kotlin code with our
+      Annotation Web Service.
+    </p>
+    <div class="flex flex-col gap-4 content-center w-full">
+      <textarea
+        id="code-input"
+        name="code-input"
+        rows="10"
+        class="border-2 border-orange-400 p-2 w-full resize-none"
+      >
+public static void main(String[] args) {}
+      </textarea>
+
+      <div class="flex justify-center gap-2 w-min mx-auto">
+        <select name="language-select" id="language-select">
+          <option value="java">Java</option>
+          <option value="python">Python</option>
+          <option value="kotlin">Kotlin</option>
+        </select>
+        <p class="text-center w-min p-2 mx-auto border-2 border-orange-500">
+          Translate
+        </p>
+      </div>
+
+      <textarea
+        id="code-output"
+        name="code-output"
+        rows="10"
+        disabled
+        class="border-2 border-orange-400 p-2 w-full resize-none"
+      >
+      </textarea>
+    </div>
+  </main>
+</template>
