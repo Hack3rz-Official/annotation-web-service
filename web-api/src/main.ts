69a3064fe52d1e950c216f5430358106ef1fd97e
michi <michi.blum@hotmail.com>
2022-04-09 14:28:18 +0200

    enable CORS to allow request from demo-frontend

--- web-api/src/main.ts
+++ web-api/src/main.ts
@@ -6,6 +6,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: true
+  })
   const config: ConfigService = app.get(ConfigService);
 
   await app.listen(config.get('service.port'));
