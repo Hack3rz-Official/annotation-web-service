295635fa5fbc4dbe8c42de0d8673e1beb7a09f48
michi <michi.blum@hotmail.com>
2022-04-09 17:34:03 +0200

    switch to real-time highlighting

--- demo-frontend/src/views/HomeView.vue
+++ demo-frontend/src/views/HomeView.vue
@@ -1,31 +1,68 @@
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
 import axios from "axios";
 
 const inputCode = ref("public static void main(String[] args) {}");
 const languageOptions = ref(["java", "python3", "kotlin"]);
 const selectedLanguage = ref("java");
+const isLoading = ref(false);
+
+watch(selectedLanguage, async (newSelectedLanguage, oldSelectedLanguage) => {
+  if (newSelectedLanguage == oldSelectedLanguage) {
+    return;
+  }
+  loadRandomCode();
+});
+
+watch(inputCode, async (newInputCode, oldInputCode) => {
+  if (isLoading.value) { return }
+  highlight()
+});
 
 function highlight() {
+  isLoading.value = true;
   let data = {
     code: inputCode.value,
     language: selectedLanguage.value,
   };
   let outputElem = document.getElementById("code-output");
-  outputElem.innerHTML = null;
   axios
     .post("http://localhost:3000/highlight", data)
     .then((response) => {
       let newElement = document
         .createRange()
         .createContextualFragment(response.data);
+      outputElem.innerHTML = null;
       outputElem.appendChild(newElement);
+      isLoading.value = false;
     })
     .catch((error) => {
       console.log(error);
       outputElem.innerHTML = "Error in Highlighting Service";
+      isLoading.value = false;
     });
 }
+
+onMounted(() => {
+  loadRandomCode();
+
+  document
+    .getElementById("code-input")
+    .addEventListener("keydown", function (e) {
+      if (e.key == "Tab") {
+        e.preventDefault();
+        var start = this.selectionStart;
+        var end = this.selectionEnd;
+
+        // set textarea value to: text before caret + tab + text after caret
+        this.value =
+          this.value.substring(0, start) + "\t" + this.value.substring(end);
+
+        // put caret at right position again
+        this.selectionStart = this.selectionEnd = start + 1;
+      }
+    });
+});
 
 function loadRandomCode() {
   let randomCode = getRandomCode();
@@ -72,18 +109,15 @@ print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))`;
 }`;
   }
 }
-
-onMounted(() => {
-  loadRandomCode();
-});
 </script>
 
 <template>
   <main class="container mx-auto px-3 gap-y-3">
     <!-- <h1 class="text-2xl py-4">Syntax Highlighting</h1> -->
-    <p class="py-6 text-lg">
-      Frontend Demo for the Annotation Web Service enabling fast code syntax highlighting. <br>
-      Supported languages are Java, Python3 and Kotlin. 
+    <p class="py-6 text-xl">
+      Frontend Demo for the Annotation Web Service enabling fast code syntax
+      highlighting. <br />
+      Supported languages are Java, Python3 and Kotlin.
     </p>
 
     <div class="w-full flex flex-wrap justify-start gap-4 mb-4">
@@ -91,12 +125,7 @@ onMounted(() => {
       <select
         name="language-select"
         id="language-select"
-        class="
-            px-2
-            bg-cyan-800
-            text-white
-            hover:bg-cyan-600
-            hover:text-white"
+        class="p-2 bg-cyan-800 text-white hover:bg-cyan-600 hover:text-white"
         v-model="selectedLanguage"
       >
         <option
@@ -107,7 +136,7 @@ onMounted(() => {
           {{ language }}
         </option>
       </select>
-      <div
+      <!-- <div
         class="
           border-2
           p-2
@@ -120,40 +149,30 @@ onMounted(() => {
         @click="loadRandomCode"
       >
         Load random code snippet
-      </div>
+      </div> -->
     </div>
 
-    <div class="flex flex-col gap-4 content-center w-full lg:flex-row">
+    <div class="flex flex-col gap-4 content-center w-full xl:flex-row">
       <textarea
         id="code-input"
         name="code-input"
         rows="10"
-        class="bg-white border-2 border-cyan-800 p-2 w-full resize-none"
+        class="
+          bg-white
+          border-2 border-cyan-800
+          p-2
+          w-full
+          resize-none
+          font-mono
+          h-96
+        "
         v-model="inputCode"
       ></textarea>
-
-      <div class="flex justify-center gap-2 w-min mx-auto lg:flex-col">
-        <p
-          class="
-            text-center
-            w-min
-            p-2
-            mx-auto
-            bg-cyan-800
-            text-white
-            hover:bg-cyan-600 hover:text-white
-            cursor-pointer
-          "
-          @click="highlight"
-        >
-          Highlight
-        </p>
-      </div>
 
       <div
         id="code-output"
         name="code-output"
-        class="bg-slate-50 border-2 border-cyan-800 p-2 w-full"
+        class="bg-slate-50 border-2 border-cyan-800 p-2 w-full h-96 overflow-auto"
       ></div>
     </div>
   </main>
