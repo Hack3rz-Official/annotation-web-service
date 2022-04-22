91c65e445d9971e50af7e917fdcaa13c7ad1ddb2
michi <michi.blum@hotmail.com>
2022-04-22 11:13:23 +0200

    refactor: move file preview in seperate component

--- demo-frontend/src/router/index.js
+++ demo-frontend/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import DemoView from '../views/DemoView.vue'
+import Home from '../views/Home.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: DemoView
+      component: Home
     },
     {
       path: '/demo-old',
