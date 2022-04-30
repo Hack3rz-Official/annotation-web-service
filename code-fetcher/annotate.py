ccf12f29a79eba503cc0c43bc3954820a4dfcffa
miczed <mighty.miczed@gmail.com>
2022-04-30 14:08:00 +0200

    Update code-fetcher to use command-line args instead of env variables

--- code-fetcher/annotate.py
+++ code-fetcher/annotate.py
@@ -1,21 +1,31 @@
 import os
 import requests
-from dotenv import load_dotenv
+import math
 
-load_dotenv()
+def convert_size(size_bytes):
+    if size_bytes == 0:
+        return "0B"
+    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p, 2)
+    return "%s %s" % (s, size_name[i])
 
-def do_annotation(language):
+
+def do_annotation(language, url):
     for root, dirs, filenames in os.walk(language):
-        for filename in filenames:
+        for index, filename in enumerate(filenames):
             with open(language + "/" + filename, "r") as language_file:
                 try:
-                    print("Annotating... ", filename)
+                    print(F"Annotating {language} file {index}/{len(filenames)}: {filename}")
                     code = language_file.read()
+                    locs = len(language_file.readlines())
                     body = {
-                        "language": language.upper(),
+                        "language": language.lower(),
                         "code": code
                     }
-                    res = requests.post(url=os.environ.get('ANNOTATE_URL'), json=body)
+                    res = requests.post(url, json=body)
+                    print(f"Size: {convert_size(os.path.getsize(language_file.name))} LOCs: {locs} Done in: {res.elapsed.total_seconds()}s")
                     if res.status_code != 200:
                         print("Error during annotation: ", res.text)
                 except Exception as e:
--- code-fetcher/main.py
+++ code-fetcher/main.py
@@ -1,9 +1,29 @@
+import argparse
 from download import download_files_from_github
 from annotate import do_annotation
 
-# supported languages: "java", "python", "kotlin"
-languages = ["java", "python", "kotlin"]
-for language in languages:
-    download_files_from_github(language)
-    do_annotation(language)
+def main():
 
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-t', '--token', help="Github Access Token", required=True)
+
+    parser.add_argument('-f', '--files', type=int, help="number of files", default=1000)
+
+    parser.add_argument('-u', '--url', help="api url", default="http://localhost:3000/highlight")
+
+    args = parser.parse_args()
+
+    github_token = args.token
+    max_files = args.files
+    url = args.url
+
+    # supported languages: "java", "python", "kotlin"
+    languages = ["java", "python3", "kotlin"]
+    for language in languages:
+        download_files_from_github(language, github_token, max_files)
+        do_annotation(language, url)
+
+
+if __name__ == "__main__":
+    main()
