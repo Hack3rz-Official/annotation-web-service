e10e57d3108c878612de776ff24b5948386228e1
miczed <mighty.miczed@gmail.com>
2022-04-30 14:36:21 +0200

    Make persisting of code async in annotation-service

--- annotation-service/src/main/java/com/hack3rz/annotationservice/ApplicationConfiguration.java
+++ annotation-service/src/main/java/com/hack3rz/annotationservice/ApplicationConfiguration.java
@@ -1,4 +1,9 @@
 package com.hack3rz.annotationservice;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
 public class ApplicationConfiguration {
 }
--- annotation-service/src/main/java/com/hack3rz/annotationservice/service/AnnotationService.java
+++ annotation-service/src/main/java/com/hack3rz/annotationservice/service/AnnotationService.java
@@ -2,10 +2,14 @@ package com.hack3rz.annotationservice.service;
 
 import com.hack3rz.annotationservice.enumeration.SupportedLanguage;
 import com.hack3rz.annotationservice.exception.ApiException;
+import com.hack3rz.annotationservice.model.Annotation;
+import com.hack3rz.annotationservice.repository.AnnotationRepository;
 import lexer.HTok;
 import lexer.LTok;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import resolver.JavaResolver;
 import resolver.KotlinResolver;
@@ -28,6 +32,9 @@ public class AnnotationService {
     private static final Logger log = LoggerFactory.getLogger(AnnotationService.class);
     private static final String INSTANTIATED_RESOLVER_OF_TYPE = "Instantiated resolver of type {}";
     private static final String COULD_NOT_BE_HIGHLIGHTED = "Code could not be highlighted.";
+
+    @Autowired
+    private AnnotationRepository repository;
 
     public LTok[] lexCode(String code, SupportedLanguage language) {
         log.info("Received code to annotate...");
@@ -80,6 +87,26 @@ public class AnnotationService {
 
         return htmlCode;
     }
+
+    @Async
+    public void persistCode(LTok[] lexingTokens, String code, SupportedLanguage language) {
+        HTok[] highlightingTokens = highlightCode(code, language);
+
+        // only for debugging purposes and to compare our solution to the java highlighter
+        String htmlCode = debugHighlightCode(code, language);
+
+        // Store in database
+        Annotation.AnnotationKey annotationKey = new Annotation.AnnotationKey(language, pluckTokenIds(highlightingTokens));
+        Annotation annotation = Annotation.builder()
+                .id(annotationKey)
+                .sourceCode(code)
+                .highlightingTokens(pluckHCodeValues(highlightingTokens))
+                .highlightingCode(htmlCode)
+                .build();
+
+        repository.save(annotation);
+    }
+
 
     public List<Integer> pluckTokenIds(LTok[] lToks) {
         return Arrays.stream(lToks).map(lTok -> lTok.tokenId).collect(Collectors.toList());
