84286b486ef1043874848327730ebc643278efb1
Nicola Crimi <crimi.nicola@gmail.com>
2022-05-05 08:58:16 +0200

    commented test code and expanded some tests

--- training-service/test/test_training_service.py
+++ training-service/test/test_training_service.py
@@ -12,7 +12,14 @@ import numpy as np
 
 class TrainingServiceTest(Hack3rzTest):
 
-    def test_data_preprocessing(self):  
+    def test_data_preprocessing(self):
+        """Loads java data to test the data_preprocessing function. Should split the lexing and highlighting tokens & separates them.
+        Then it will be asserted that neither of X or T are empty and both arrays are np.ndarrays (important for subsequent steps).
+        
+        Args: 
+            training_data represents fetched training data from the db.
+        
+        """
         training_data = self.annotation_repository.find_training_data("java")
         X, T = data_preprocessing(training_data)
         self.assertIsInstance(X, np.ndarray)
@@ -22,6 +29,16 @@ class TrainingServiceTest(Hack3rzTest):
 
 
     def test_split_data(self):
+        """Tests for a correct data split in training and validation data. After the split it will be asserted that 
+        the shapes are correct and from same size for X_train, T_train, X_val, T_val.
+
+        Args:
+            training_data represents fetched training data from the db.
+
+        Returns:
+            4 arrays with default split of 0.8 training & 0.2 validation data
+        
+        """
         training_data = self.annotation_repository.find_training_data("java")
         X, T = data_preprocessing(training_data)
         X_train, X_val, T_train, T_val = split_data(X, T, train_percentage=0.8)
@@ -30,15 +47,29 @@ class TrainingServiceTest(Hack3rzTest):
 
     @patch('src.util.SHModelUtils.SHModel.predict')
     def test_compute_accuracy_valid(self, predict_mock):
+        """Tests accuracy computation while mocking the return value of the predict function. Since 10 values are inside
+        the training and the testing array, with only one deviation the logical return value of the compute_accuracy value
+        must be 0.9.
+
+        Args:
+            predict_mock which is an array of dimension 1x10 and can be used to compute accuracy.
+            best_sh_model which is the current SHModel which is in use by the prediction service and two arrays, training and tesing set.
+        
+        Returns:
+            Float number in range [0,1].
+        
+        """
         X = np.array([[1], [1], [1], [1],[1],[1],[1],[1],[1],[1]])
         T = np.array([[1], [2], [1], [1],[1],[1],[1],[1],[1],[1]])
         predict_mock.side_effect = [[1], [1], [1], [1],[1],[1],[1],[1],[1],[1]]
         best_sh_model = SHModel("java", "test_best")
         ratio = compute_accuracy(best_sh_model, X, T)
         self.assertEqual(ratio, 0.9)
+        self.assertTrue(0 <= ratio <= 1)
     
-    @patch('src.util.SHModelUtils.SHModel.predict')
-    def test_compute_accuracy_invalid(self, predict_mock):
+    def test_compute_accuracy_invalid(self):
+        """Tests if an assertion error is raised by the compute_accuracy function if the two arrays are not of the same shape.
+        """
         X = np.array([[1], [1], [1], [1],[1],[1],[1],[1],[1],[1]])
         T = np.array([[1], [2], [1], [1],[1],[1],[1],[1],[1]])
         best_sh_model = SHModel("java", "test_best")
@@ -46,6 +77,16 @@ class TrainingServiceTest(Hack3rzTest):
         
 
     def test_shuffle_data(self):
+        """Tests if the input and ouput arrays are not equal after calling the shuffle_data function. Before the
+        shuffeling the data_preprocessing function is called to split the data into training and testing data. 
+
+        Args:
+            training_data which are lexing and highlighting tokens from the aws_test db.
+
+        Returns:
+            Two shuffled arrays.
+        
+        """
         training_data = self.annotation_repository.find_training_data("java")
         X,T = data_preprocessing(training_data)
         X_shuff,T_shuff = shuffle_data(X,T)
@@ -55,6 +96,20 @@ class TrainingServiceTest(Hack3rzTest):
 
     @patch('src.util.SHModelUtils.SHModel.finetune_on')
     def test_train(self, finetune_on_mock):
+        """Tests the training process. Mocks a return value of the finetune_on function (1).
+        Since its difficult to test the actual training process, we count the number of finetune_on calls 
+        the model is generating during the process. The seeked number should be batch * number of samples. If the function is called
+        batch * number of samples times then the test was successful. 
+
+        Args:
+            finetune_mock is an integer 1 which is used for the above explained purposes.
+            best_sh_model which is a newly instantiated SHModel, a training set with training points and targets and a fixed number of
+            epochs (default is 10).
+
+        Returns:
+            Array with losses (if assigned)
+
+        """
         finetune_on_mock.return_value = 1
 
         best_sh_model = SHModel("java", "test_best")
@@ -72,6 +127,18 @@ class TrainingServiceTest(Hack3rzTest):
     @patch('src.services.training.compute_accuracy')
     @patch('src.services.training.train')
     def test_improve_model_do_nothing(self, train_mock, compute_accuracy_mock):
+        """Test for the case where a model (fetched from db) is trained and compared with the one in use (from prediction
+        service). In this case the fetched model`s accuary is NOT higher than the one in use and therefore the current model
+        is kept while the fetched model is discarded. The whole test case is built indirectly. We fetch before and after
+        the improve_model() function the same amount of training_data (10, which is the number of entries when aws_test db is 
+        instantiated). Consequently, this means that the new accuray is lower (5) than the current acccuracy (10) and there is no
+        change of models. Evenutally, this implies that the training data is not marked as "used for training" and thus 
+        the desired state is fact.
+
+        Args:
+            train_mock which is an empty array. compute_accuracy_mock which is an array with two values.
+
+        """
         # create functions mocks
         # skip train method execution
         train_mock.return_value = []
@@ -95,6 +162,18 @@ class TrainingServiceTest(Hack3rzTest):
     @patch('src.services.training.compute_accuracy')
     @patch('src.services.training.train')
     def test_improve_model_update_best_model(self, train_mock, compute_accuracy_mock):
+        """Test for the case where a model (fetched from db) is trained and compared with the one in use (from prediction
+        service). In this case the fetched model`s accuary is higher than the one in use and therefore the current model
+        is discared and the new one saved to the current repository. The whole test case is built indirectly. We fetch before and after
+        the improve_model() function the same amount of training_data (10, which is the number of entries when aws_test db is 
+        instantiated). Consequently, this means that the new accuray is higher (10) than the current acccuracy (6) and there is a
+        change of models. Evenutally, this implies that the training data is marked as "used for training" and thus does not appear
+        as training_data anymore when fetched from the db via the function find_training_data.
+
+        Args:
+            train_mock which is an empty array. compute_accuracy_mock which is an array with two values.
+
+        """
         # create functions mocks
         # skip train method execution
         train_mock.return_value = []
