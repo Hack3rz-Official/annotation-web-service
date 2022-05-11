2b33046ac030e440fbd82f270324f4f69e8e2817
michi <michi.blum@hotmail.com>
2022-05-11 21:25:29 +0200

    use settingsStore to make batch parameters dynamic

--- demo-frontend/src/composables/axios.js
+++ demo-frontend/src/composables/axios.js
@@ -1,55 +1,50 @@
 import axios from "axios";
-
-const MAX_REQUESTS_COUNT = 1;
-const INTERVAL_MS = 10;
-let PENDING_REQUESTS = 0;
+import { useSettingsStore } from "../stores/settingsStore";
 
 // default axios instance
 export const axiosDefault = axios.create({});
 
-// create request limited axios instance 
+// create request limited axios instance
 export const axiosLimited = axios.create({});
 
-// Axios Request Interceptor
-axiosLimited.interceptors.request.use(function (config) {
-  return new Promise((resolve, reject) => {
-    let interval = setInterval(() => {
-      if (PENDING_REQUESTS < MAX_REQUESTS_COUNT) {
-        PENDING_REQUESTS++;
-        console.log(`Request sent - Pending requests: ${PENDING_REQUESTS}`);
-        config.meta = config.meta || {};
-        config.meta.requestStartedAt = new Date().getTime();
-        console.log({ config });
-        clearInterval(interval);
-        resolve(config);
-      }
-    }, INTERVAL_MS);
+/*
+* This timeout wrapper is a workaround to use pinia store outside of components.
+* https://pinia.vuejs.org/core-concepts/outside-component-usage.html#single-page-applications
+*/
+setTimeout(() => {
+  const settingsStore = useSettingsStore();
+
+  // Axios Request Interceptor
+  axiosLimited.interceptors.request.use(function (config) {
+    return new Promise((resolve, reject) => {
+      let interval = setInterval(() => {
+        if (settingsStore.pendingRequests < settingsStore.maxRequests) {
+          settingsStore.pendingRequests++;
+          console.log(`Request sent - Pending requests: ${settingsStore.pendingRequests}`);
+          config.meta = config.meta || {};
+          config.meta.requestStartedAt = new Date().getTime();
+          console.log({ config });
+          clearInterval(interval);
+          resolve(config);
+        }
+      }, settingsStore.interval);
+    });
   });
-});
 
-// Axios Response Interceptor
-axiosLimited.interceptors.response.use(
-  function (response) {
-    PENDING_REQUESTS = Math.max(0, PENDING_REQUESTS - 1);
-    console.log(`Request resolved - Pending requests: ${PENDING_REQUESTS}`);
-    console.log({ response });
-    return Promise.resolve(response);
-  },
-  function (error) {
-    PENDING_REQUESTS = Math.max(0, PENDING_REQUESTS - 1);
-    console.log(`Request resolved - Pending requests: ${PENDING_REQUESTS}`);
-    return Promise.reject(error);
-  }
-);
-
-axiosLimited.interceptors.response.use(
-  (x) => {
-    x.responseTime = new Date().getTime() - x.config.meta.requestStartedAt;
-    return x;
-  },
-  // Handle 4xx & 5xx responses
-  (x) => {
-    x.responseTime = new Date().getTime() - x.config.meta.requestStartedAt;
-    return x;
-  }
-);
+  // Axios Response Interceptor
+  axiosLimited.interceptors.response.use(
+    function (response) {
+      settingsStore.pendingRequests = Math.max(0, settingsStore.pendingRequests - 1);
+      response.responseTime = new Date().getTime() - response.config.meta.requestStartedAt;
+      console.log(`Request resolved - Pending requests: ${settingsStore.pendingRequests}`);
+      console.log({ response });
+      return Promise.resolve(response);
+    },
+    function (error) {
+      settingsStore.pendingRequests = Math.max(0, settingsStore.pendingRequests - 1);
+      response.responseTime = new Date().getTime() - response.config.meta.requestStartedAt;
+      console.log(`Request resolved - Pending requests: ${settingsStore.pendingRequests}`);
+      return Promise.reject(error);
+    }
+  );
+}, 1);
---
+++ demo-frontend/src/stores/settingsStore.js
@@ -0,0 +1,14 @@
+import { defineStore } from "pinia";
+
+export const useSettingsStore = defineStore({
+  id: "settings",
+  state: () => ({
+      maxRequests: 1, // how many concurrent requests are allowed for highlighting
+      pendingRequests: 0, // amount of currently pending requests
+      interval: 10, // ms before next request is tried
+  }),
+
+  getters: {},
+
+  actions: {},
+});
