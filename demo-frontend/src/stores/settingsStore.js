32e3379af134de2a56fa9728f87193e9baf0fb35
michi <michi.blum@hotmail.com>
2022-05-11 23:54:30 +0200

    loc - time scatterplot

--- demo-frontend/src/stores/settingsStore.js
+++ demo-frontend/src/stores/settingsStore.js
@@ -3,7 +3,7 @@ import { defineStore } from "pinia";
 export const useSettingsStore = defineStore({
   id: "settings",
   state: () => ({
-      maxRequests: 1, // how many concurrent requests are allowed for highlighting
+      maxRequests: 4, // how many concurrent requests are allowed for highlighting
       pendingRequests: 0, // amount of currently pending requests
       interval: 10, // ms before next request is tried
   }),
